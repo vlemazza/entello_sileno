c634724544748e514d01e4dea2ef3c4b301903f8
giuelino <rgiulio59@gmail.com>
2026-02-21 14:32:36 +0100

    Aggiunto il cooldown per le parole meme

--- src/utils/text_meme.py
+++ src/utils/text_meme.py
@@ -1,5 +1,7 @@
 from telegram import Update
 import os
+import time
+from utils.logger import debug
 
 MEME = {
     "banana.opus": ["🍌", "banana", "banan"],
@@ -8,18 +10,28 @@ MEME = {
     "stelio.opus": ["stelio", "kontos"],
 }
 
+COOLDOWN_GROUPS = {}
+COOLDOWN_SECONDS = 300
+
+
 async def check_meme(update, text):
     text = text.lower()
-
+    actual_time=time.time()
     for audio_file, triggers in MEME.items():
         if any(trigger in text for trigger in triggers):
-            if os.path.exists(audio_file):
-                with open(audio_file, "rb") as audio:
-                    await update.message.reply_audio(
-                        audio=audio,
-                        reply_to_message_id=update.message.message_id
-                    )
+            last_sent_meme_key=(update.effective_chat.id, audio_file)
+            last_sent_meme=COOLDOWN_GROUPS.get(last_sent_meme_key, 0)
+            if(actual_time - last_sent_meme >= COOLDOWN_SECONDS):
+                if os.path.exists(audio_file):
+                    with open(audio_file, "rb") as audio:
+                        COOLDOWN_GROUPS[last_sent_meme_key]=actual_time
+                        await update.message.reply_audio(
+                            audio=audio,
+                            reply_to_message_id=update.message.message_id
+                        )
+                else:
+                    await update.message.reply_text(f"{audio_file} not found")
             else:
-                await update.message.reply_text(f"{audio_file} not found")
+                return
 
             return
